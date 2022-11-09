755cef7796cf2cb600ac8fba8843ea3be0f9233e
hasan-furkan <hsnfrkn32@gmail.com>
2022-11-09 10:52:33 +0300

    struct

--- main.go
+++ main.go
@@ -35,33 +35,63 @@ import "fmt"
 
 // func main() {
 // 	fmt.Println(topla(2, 5)) //2+5 sonucunu ekrana bastır
+
+// fmt.Println(dortISLEM(10))
+// topla, cikar, carp, _ := dortISLEM(10)
+// fmt.Println(topla, cikar, carp)
+// metin := "Merhaba Dünya"
+
+// func(a string) {
+// 	fmt.Println(a)
+// }(metin)
+
+// for i := 0; i < 10; i++ {
+// 	fmt.Println(i)
 // }
 
-func dortISLEM(sayi int) (x, y, a, d int) {
-	x = sayi + 5
-	y = sayi / 5
-	a = sayi * 5
-	d = sayi - 5
-	return x, y, a, d
+// deger := 0
+// for deger < 10 {
+// 	fmt.Println("deger", deger)
+// 	deger++
+// }
+
+// var a int = 8
+// ekle(&a)
+// fmt.Println(a)
+// }
+
+// func dortISLEM(sayi int) (x, y, a, d int) {
+// 	x = sayi + 5
+// 	y = sayi / 5
+// 	a = sayi * 5
+// 	d = sayi - 5
+// 	return x, y, a, d
+// }
+
+// func ekle(v *int) {
+// 	*v += 5
+// }
+
+type user struct {
+	name    string
+	surname string
+	age     int
 }
 
 func main() {
-	// fmt.Println(dortISLEM(10))
-	// topla, cikar, carp, _ := dortISLEM(10)
-	// fmt.Println(topla, cikar, carp)
-	// metin := "Merhaba Dünya"
 
-	// func(a string) {
-	// 	fmt.Println(a)
-	// }(metin)
+	// user1 := user{"hasan furkan", "koprulu", 21}
+	// user1.name = "ihsan"
+	// user1.surname = "cetin"
+	// user1.age = 55
+	// fmt.Println(user1)
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	// user1 := user{}
+	// user1.name = "hasan"
+	// user1.surname = "furkan"
+	// user1.name, user1.surname = "hasan furkan", "koprulu"
+	// user1.age = 21
 
-	deger := 0
-	for deger < 10 {
-		fmt.Println("deger", deger)
-		deger++
-	}
+	user1 := user{age: 21, surname: "koprulu"}
+	fmt.Println(user1)
 }
